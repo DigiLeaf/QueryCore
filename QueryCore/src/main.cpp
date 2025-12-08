79af64ef544214cb2a3ccec430cdbd2a8cc8c6f5
DigiLeaf <leiforth01@hotmail.com>
2025-12-07 18:23:20 -0600

    Added an easily expandable main menu

--- QueryCore/src/main.cpp
+++ QueryCore/src/main.cpp
@@ -4,8 +4,7 @@
 //Header Files
 #include "../include/FileCrawler.h"
 
-int main() {
-
+void testFilecrawl() {
 	//relative filepath short because relative to where program is executed (project root)
 	std::string filepath = "data";
 	FileCrawler crawler(filepath);
@@ -16,7 +15,50 @@ int main() {
 	for (const std::string file : files) {
 		std::cout << file << std::endl;
 	}
+}
 
-	std::cout << "We crawled through the data folder";
+int mainMenu() {
+	int choice;
+	bool running = true;
+
+	do {
+		std::cout << "#############" << std::endl;
+		std::cout << "# QueryCore #" << std::endl;
+		std::cout << "#############" << std::endl << std::endl;;
+
+		std::cout << "1. Test FileCrawler" << std::endl;
+		std::cout << "2. Exit" << std::endl;
+		std::cout << std::endl << "Enter an option choice:" << std::endl;
+
+		std::cin >> choice;
+		
+		switch (choice) {
+		case 1:
+			std::cout << "\n";
+			std::cout << "###################" << std::endl;
+			std::cout << " File Crawl Chosen" << std::endl;
+			std::cout << "###################" << std::endl << std::endl;
+
+			testFilecrawl();
+			std::cout << "\n\n";
+			break;
+		case 2:
+			running = false;
+			std::cout << "Exiting the program..." << std::endl;
+			return 0;
+		default:
+			std::cout << "Please enter a valid choice. Please Try Again." << std::endl;
+			break;
+		}
+	} while (running);
+	
+}
+
+
+int main() {
+
+	mainMenu();
+
+	std::cout << "we are exiting the main function. goodbye.";
 	return 0;
 }
